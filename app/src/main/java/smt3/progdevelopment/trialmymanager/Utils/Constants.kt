e93267419eabbd00b04dfc8fd7186b83ac4dc785
Ahmad Ramadhani <49054386+dhani-rama@users.noreply.github.com>
2021-09-12 09:40:17 +0700

    add constants file and image

---
+++ app/src/main/java/smt3/progdevelopment/trialmymanager/Utils/Constants.kt
@@ -0,0 +1,12 @@
+package smt3.progdevelopment.trialmymanager.Utils
+
+object Constants {
+    const val LOGIN = "signIn"
+    const val USER = "user"
+    const val USER_ID = "id"
+    const val USER_NAME = "name"
+    const val USER_PHONE = "phone"
+    const val USER_PASSWORD = "password"
+    const val USER_EMAIL = "email"
+
+}
